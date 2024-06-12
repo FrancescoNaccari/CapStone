457807665f8c38fa4ea492626ee677db8da20366
FrancescoNaccari <francesco.n92@gmail.com>
2024-06-12 16:47:07 +0200

    elimimazione componets prova , piccolo ritocchi navbar e font family

--- PROGETTOFINALE/n3/Tranig/src/app/app.module.ts
+++ PROGETTOFINALE/n3/Tranig/src/app/app.module.ts
@@ -7,26 +7,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Error404Component } from './components/error404/error404.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { GraficChartsComponent } from './components/grafic-charts/grafic-charts.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { WebsocketComponent } from './components/websocket/websocket.component';
-import { GraficoFinanziarioComponent } from './components/grafico-finanziario/grafico-finanziario.component';
-import { FinanziarioComponent } from './components/finanziario/finanziario.component';
+
 import { BorsaComponent } from './components/borsa/borsa.component';
 import { StockCardComponent } from './components/stock-card/stock-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-     GraficChartsComponent,
+
     NavbarComponent,
     Error404Component,
     HomeComponent,
     WebsocketComponent,
     FooterComponent,
-    GraficoFinanziarioComponent,
-    FinanziarioComponent,
+
     BorsaComponent,
     StockCardComponent
   ],
