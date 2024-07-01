83ffa998deed61d4a6c2bdba10e57640f4f0e623
FrancescoNaccari <francesco.n92@gmail.com>
2024-07-01 13:19:21 +0200

    fix-404

--- PROGETTOFINALE/n3/Tranig/src/app/components/error404/error404.component.ts
+++ PROGETTOFINALE/n3/Tranig/src/app/components/error404/error404.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-error404',
   templateUrl: './error404.component.html',
   styleUrls: ['./error404.component.scss']
 })
-export class Error404Component {
+export class Error404Component implements OnInit {
+  countdown: number = 7;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Timer per il conto alla rovescia e reindirizzamento
+    const timer$ = interval(1000).pipe(take(this.countdown));
+
+    timer$.subscribe({
+      next: (value) => this.countdown -= 1,
+      complete: () => this.router.navigate(['/home'])
+    });
+  }
 
 }
