ece2bedd0df9d19d6a6a69794570c1e3709e2389
FrancescoNaccari <francesco.n92@gmail.com>
2024-07-01 16:49:32 +0200

    FIX- SPINER VARI

--- PROGETTOFINALE/n3/Tranig/src/app/components/favorite-stocks/favorite-stocks.component.ts
+++ PROGETTOFINALE/n3/Tranig/src/app/components/favorite-stocks/favorite-stocks.component.ts
@@ -176,6 +176,80 @@
 // function isAuthData(user: any): user is AuthData {
 //   return user && 'accessToken' in user && 'user' in user;
 // }
+// import { Component, OnInit } from '@angular/core';
+// import { AuthData } from 'src/app/interface/auth-data.interface';
+// import { FavoriteStock } from 'src/app/interface/favorite-stock.interface';
+// import { AuthService } from 'src/app/service/auth.service';
+// import { FavoriteStockService } from 'src/app/service/favorite-stock.service';
+// import { TranslateService } from '@ngx-translate/core';
+
+// @Component({
+//   selector: 'app-favorite-stocks',
+//   templateUrl: './favorite-stocks.component.html',
+//   styleUrls: ['./favorite-stocks.component.scss']
+// })
+// export class FavoriteStocksComponent implements OnInit {
+//   favoriteStocks: FavoriteStock[] = [];
+//   user: AuthData | null = null;
+//   errorMessage: string | null = null;
+//   alertMessage: string | null = null;
+//   alertType: string = 'info';
+
+//   constructor(
+//     private favoriteStockService: FavoriteStockService,
+//     private authSrv: AuthService,
+//     private translate: TranslateService
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.authSrv.user$.subscribe((data) => {
+//       if (isAuthData(data)) {
+//         this.user = data;
+//         if (this.user.user.idUtente) {
+//           this.loadFavorites(this.user.user.idUtente.toString());
+//         }
+//       }
+//     });
+//   }
+
+//   loadFavorites(userId: string): void {
+//     this.favoriteStockService.getFavorites(parseInt(userId)).subscribe(
+//       (favorites: FavoriteStock[]) => {
+//         this.favoriteStocks = favorites;
+//       },
+//       (error) => {
+//         this.errorMessage = this.translate.instant('favoriteStocks.LOAD_ERROR');
+//       }
+//     );
+//   }
+
+//   removeFavorite(stock: FavoriteStock): void {
+//     if (stock.id) {
+//       this.favoriteStockService.removeFavorite(stock.id).subscribe(
+//         () => {
+//           this.favoriteStocks = this.favoriteStocks.filter(s => s.id !== stock.id);
+//           this.showAlert(
+//             this.translate.instant('alerts.REMOVED_FROM_FAVORITES', { stockName: stock.name }),
+//             'warning'
+//           );
+//         },
+//         (error) => {
+//           this.errorMessage = this.translate.instant('favoriteStocks.REMOVE_ERROR');
+//         }
+//       );
+//     }
+//   }
+
+//   showAlert(message: string, type: string): void {
+//     this.alertMessage = message;
+//     this.alertType = type;
+//     setTimeout(() => this.alertMessage = null, 3000); // Nasconde l'alert dopo 3 secondi
+//   }
+// }
+
+// function isAuthData(user: any): user is AuthData {
+//   return user && 'accessToken' in user && 'user' in user;
+// }
 import { Component, OnInit } from '@angular/core';
 import { AuthData } from 'src/app/interface/auth-data.interface';
 import { FavoriteStock } from 'src/app/interface/favorite-stock.interface';
@@ -194,6 +268,7 @@ export class FavoriteStocksComponent implements OnInit {
   errorMessage: string | null = null;
   alertMessage: string | null = null;
   alertType: string = 'info';
+  isLoading: boolean = true; // Aggiungi questa variabile
 
   constructor(
     private favoriteStockService: FavoriteStockService,
@@ -216,9 +291,11 @@ export class FavoriteStocksComponent implements OnInit {
     this.favoriteStockService.getFavorites(parseInt(userId)).subscribe(
       (favorites: FavoriteStock[]) => {
         this.favoriteStocks = favorites;
+        this.showSpinnerForOneSecond();
       },
       (error) => {
         this.errorMessage = this.translate.instant('favoriteStocks.LOAD_ERROR');
+        this.isLoading = false; // Nascondi lo spinner in caso di errore
       }
     );
   }
@@ -238,6 +315,13 @@ export class FavoriteStocksComponent implements OnInit {
         }
       );
     }
+  }
+
+  showSpinnerForOneSecond(): void {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 500);
   }
 
   showAlert(message: string, type: string): void {
--- PROGETTOFINALE/n3/Tranig/src/app/components/home/home.component.ts
+++ PROGETTOFINALE/n3/Tranig/src/app/components/home/home.component.ts
@@ -273,6 +273,154 @@
 // return user && 'accessToken' in user && 'user' in user;
 // }
 
+// import { Component, HostListener, OnInit } from '@angular/core';
+// import { AuthData } from 'src/app/interface/auth-data.interface';
+// import { FavoriteStock } from 'src/app/interface/favorite-stock.interface';
+// import { LogoBorsa } from 'src/app/interface/logo-borsa.interface';
+// import { LogoDto } from 'src/app/interface/logo-dto.interface';
+// import { RealTimePriceResponse } from 'src/app/interface/real-time-price-response.interface';
+// import { StockList } from 'src/app/interface/stock-list.interface';
+// import { AuthService } from 'src/app/service/auth.service';
+// import { FavoriteStockService } from 'src/app/service/favorite-stock.service';
+// import { LogoBorsaService } from 'src/app/service/logo-borsa.service';
+// import { RealTimePriceService } from 'src/app/service/real-time-price.service';
+// import { StockListService } from 'src/app/service/stock-list.service';
+// import { TranslateService } from '@ngx-translate/core';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.scss']
+// })
+// export class HomeComponent implements OnInit {
+//   stock: any = {};
+//   symbol: string = '';
+//   errorMessage: string | null = null;
+//   user: AuthData | null = null;
+//   allStocks: StockList[] = [];
+//   displayedStocks: StockList[] = [];
+//   favoriteStocks: FavoriteStock[] = [];
+
+//   constructor(
+//     private realTimePriceService: RealTimePriceService,
+//     private logoBorsaService: LogoBorsaService,
+//     private stockListService: StockListService,
+//     private authSrv: AuthService,
+//     private favoriteStockService: FavoriteStockService,
+//     private translate: TranslateService
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.getStocks();
+//     this.authSrv.user$.subscribe((data) => {
+//       if (isAuthData(data)) {
+//         this.user = data;
+//         if (this.user.user.idUtente) {
+//           this.loadFavorites(this.user.user.idUtente.toString());
+//         }
+//       }
+//     });
+//   }
+
+//   @HostListener('window:resize', ['$event'])
+//   onResize(event: any) {
+//     this.updateDisplayedStocks();
+//   }
+
+//   getStocks(): void {
+//     this.logoBorsaService.getAllLogos().subscribe(
+//       (response: LogoDto[]) => {
+//         let filteredStocks = response.filter(logo => logo.url !== null && logo.url !== '');
+//         let filter2: LogoDto[] = [];
+//         filteredStocks.forEach(logo => {
+//           let image = new Image();
+//           image.src = logo.url;
+//           image.onload = () => {
+//             filter2.push(logo);
+//           };
+//         });
+
+//         setTimeout(() => {
+//           this.stockListService.getStockList().subscribe((response: StockList[]) => {
+//             this.allStocks = response.filter(stock => {
+//               return filter2.map(logo => logo.symbol).includes(stock.symbol);
+//             });
+//             this.updateDisplayedStocks();
+//           });
+//         }, 2000);
+//       },
+//       (error) => {
+//         this.errorMessage = this.translate.instant('home.ERROR_MESSAGE');
+//         this.allStocks = [];
+//         this.updateDisplayedStocks();
+//       }
+//     );
+//   }
+
+//   updateDisplayedStocks(): void {
+//     const width = window.innerWidth;
+
+//     let numberOfCards;
+//     if (width < 768) {
+//       numberOfCards = 5;
+//     } else if (width >= 768 && width < 992) {
+//       numberOfCards = 7;
+//     } else if (width >= 992 && width < 1200) {
+//       numberOfCards = 8;
+//     } else {
+//       numberOfCards = 10;
+//     }
+
+//     const shuffled = this.allStocks.sort(() => 0.5 - Math.random());
+//     this.displayedStocks = shuffled.slice(0, numberOfCards);
+//   }
+
+//   loadFavorites(userId: string): void {
+//     this.favoriteStockService.getFavorites(parseInt(userId)).subscribe(
+//       (favorites: FavoriteStock[]) => {
+//         this.favoriteStocks = favorites;
+//       },
+//       (error) => {
+//         this.errorMessage = this.translate.instant('favoriteStocks.LOAD_ERROR');
+//       }
+//     );
+//   }
+
+//   onFavoriteToggled(stock: FavoriteStock): void {
+//     if (this.user) {
+//       const userId = this.user.user.idUtente;
+//       if (userId) {
+//         if (stock.favorite) {
+//           const favorite: FavoriteStock = { ...stock, userId };
+//           this.favoriteStockService.addFavorite(favorite).subscribe(
+//             (addedStock) => {
+//               this.favoriteStocks.push(addedStock);
+//             },
+//             (error) => {
+//               this.errorMessage = this.translate.instant('favoriteStocks.REMOVE_ERROR');
+//             }
+//           );
+//         } else {
+//           const favoriteToRemove = this.favoriteStocks.find(fav => fav.symbol === stock.symbol);
+//           if (favoriteToRemove) {
+//             this.favoriteStockService.removeFavorite(favoriteToRemove.id!).subscribe(
+//               () => {
+//                 this.favoriteStocks = this.favoriteStocks.filter(s => s.symbol !== stock.symbol);
+//               },
+//               (error) => {
+//                 this.errorMessage = this.translate.instant('favoriteStocks.REMOVE_ERROR');
+//               }
+//             );
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// function isAuthData(user: any): user is AuthData {
+//   return user && 'accessToken' in user && 'user' in user;
+// }
 import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthData } from 'src/app/interface/auth-data.interface';
 import { FavoriteStock } from 'src/app/interface/favorite-stock.interface';
@@ -300,6 +448,7 @@ export class HomeComponent implements OnInit {
   allStocks: StockList[] = [];
   displayedStocks: StockList[] = [];
   favoriteStocks: FavoriteStock[] = [];
+  isLoading: boolean = true; // Aggiungi questa variabile
 
   constructor(
     private realTimePriceService: RealTimePriceService,
@@ -328,6 +477,7 @@ export class HomeComponent implements OnInit {
   }
 
   getStocks(): void {
+    this.isLoading = true; // Mostra lo spinner
     this.logoBorsaService.getAllLogos().subscribe(
       (response: LogoDto[]) => {
         let filteredStocks = response.filter(logo => logo.url !== null && logo.url !== '');
@@ -346,13 +496,15 @@ export class HomeComponent implements OnInit {
               return filter2.map(logo => logo.symbol).includes(stock.symbol);
             });
             this.updateDisplayedStocks();
+            this.isLoading = false; // Nascondi lo spinner
           });
-        }, 2000);
+        }, 100);
       },
       (error) => {
         this.errorMessage = this.translate.instant('home.ERROR_MESSAGE');
         this.allStocks = [];
         this.updateDisplayedStocks();
+        this.isLoading = false; // Nascondi lo spinner
       }
     );
   }
