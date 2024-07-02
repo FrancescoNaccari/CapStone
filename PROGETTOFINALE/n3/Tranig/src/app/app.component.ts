595cda0c272cda5177e437ea78a152c0268cdabb
FrancescoNaccari <francesco.n92@gmail.com>
2024-07-02 20:46:22 +0200

    responsive login + grafica

--- PROGETTOFINALE/n3/Tranig/src/app/app.component.ts
+++ PROGETTOFINALE/n3/Tranig/src/app/app.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './service/auth.service';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from './service/language.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoginPage: boolean = false;
   constructor(
     private translate: TranslateService,
     private languageService: LanguageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
     this.languageService.setLanguage(this.languageService.getLanguage());
     this.authService.restore();
@@ -20,5 +23,12 @@ export class AppComponent {
 
   switchLanguage(language: string) {
     this.languageService.setLanguage(language);
+  }
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isLoginPage = (event.url === '/' || event.url.startsWith('/login'));
+      }
+    });
   }
 }
