44c9cfd6c0ccc9246ecc86b07bf18d2669ccda81
FrancescoNaccari <francesco.n92@gmail.com>
2024-07-04 13:15:49 +0200

    fix-iscrizione newsletter ita-en

--- PROGETTOFINALE/n3/Tranig/src/app/components/home/iscrizione-newsletter/iscrizione-newsletter.component.ts
+++ PROGETTOFINALE/n3/Tranig/src/app/components/home/iscrizione-newsletter/iscrizione-newsletter.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ProfiloService } from 'src/app/service/profilo.service';
+import { AuthService } from 'src/app/service/auth.service';
+import { User } from 'src/app/interface/user.interface';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-iscrizione-newsletter',
@@ -10,16 +14,40 @@ export class IscrizioneNewsletterComponent {
   privacyAccepted: boolean = false;
   successMessage: string | null = null;
   errorMessage: string | null = null;
+  currentUser: User | undefined;
+
+  constructor(
+    private profiloSrv: ProfiloService,
+    private authSrv: AuthService,
+    private translate: TranslateService
+  ) {
+    this.authSrv.user$.subscribe(user => {
+      this.currentUser = user?.user;
+      this.email = this.currentUser?.email || '';
+      this.privacyAccepted = this.currentUser?.newsletter || false;
+    });
+  }
 
   subscribeToNewsletter() {
     if (this.email && this.privacyAccepted) {
-      // Call the newsletter subscription service
-      // Here you would replace the following with an actual service call
-      console.log('Subscribing with email:', this.email);
-      this.successMessage = 'Subscribed successfully!';
-      setTimeout(() => this.successMessage = null, 3000);
+      if (this.currentUser?.email === this.email && this.currentUser?.newsletter) {
+        this.errorMessage = this.translate.instant('iscrizioneNewsletter.ALREADY_SUBSCRIBED_MESSAGE');
+        setTimeout(() => this.errorMessage = null, 3000);
+      } else {
+        this.profiloSrv.updateNewsletter(this.currentUser?.idUtente!, true).subscribe(
+          () => {
+            this.successMessage = this.translate.instant('iscrizioneNewsletter.SUCCESS_MESSAGE');
+            setTimeout(() => this.successMessage = null, 3000);
+          },
+          (error) => {
+            console.error('Subscription error', error);
+            this.errorMessage = this.translate.instant('iscrizioneNewsletter.SUBSCRIPTION_ERROR');
+            setTimeout(() => this.errorMessage = null, 3000);
+          }
+        );
+      }
     } else {
-      this.errorMessage = 'Please accept the privacy policy.';
+      this.errorMessage = this.translate.instant('iscrizioneNewsletter.PRIVACY_POLICY_ERROR');
       setTimeout(() => this.errorMessage = null, 3000);
     }
   }
