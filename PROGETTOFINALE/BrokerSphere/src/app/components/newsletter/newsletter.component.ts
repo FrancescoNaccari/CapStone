91c3be7962fbe51f1dca1de4359576340c9abf33
FrancescoNaccari <francesco.n92@gmail.com>
2024-07-05 10:32:18 +0200

    fix-gestione newsletter

--- PROGETTOFINALE/BrokerSphere/src/app/components/newsletter/newsletter.component.ts
+++ PROGETTOFINALE/BrokerSphere/src/app/components/newsletter/newsletter.component.ts
@@ -100,6 +100,63 @@
 // }
 
 
+// import { Component, OnInit } from '@angular/core';
+// import { TranslateService } from '@ngx-translate/core';
+// import { AuthData } from 'src/app/interface/auth-data.interface';
+// import { User } from 'src/app/interface/user.interface';
+// import { AuthService } from 'src/app/service/auth.service';
+// import { ProfiloService } from 'src/app/service/profilo.service';
+
+// @Component({
+//   selector: 'app-newsletter',
+//   templateUrl: './newsletter.component.html',
+//   styleUrls: ['./newsletter.component.scss']
+// })
+// export class NewsletterComponent implements OnInit {
+
+//   profilo: User | undefined;
+//   successMessage: string | null = null;
+//   errorMessage: string | null = null;
+
+//   constructor(private authSrv: AuthService, private profiloSrv: ProfiloService, private translate: TranslateService) { }
+
+//   ngOnInit(): void {
+//     this.authSrv.user$.subscribe((data) => {
+//       if (data && isAuthData(data)) {
+//         this.profilo = data.user;
+//       }
+//     });
+//   }
+
+//   updateNewsletter() {
+//     if (this.profilo?.idUtente) {
+//       this.profiloSrv.updateNewsletter(this.profilo.idUtente, this.profilo.newsletter).subscribe(
+//         (updatedUser) => {
+//           this.authSrv.updateUser(updatedUser);
+//           this.successMessage = this.translate.instant('newsletter.SUCCESS_MESSAGE');
+//           setTimeout(() => this.successMessage = null, 3000);
+//         },
+//         (error) => {
+//           console.error('Errore durante l\'aggiornamento della preferenza newsletter', error);
+//           this.errorMessage = this.translate.instant('newsletter.ERROR_MESSAGE');
+//           setTimeout(() => this.errorMessage = null, 3000);
+//         }
+//       );
+//     }
+//   }
+
+//   onNewsletterChange(event: Event) {
+//     const input = event.target as HTMLInputElement;
+//     if (this.profilo) {
+//       this.profilo.newsletter = input.checked;
+//       this.updateNewsletter();
+//     }
+//   }
+// }
+
+// function isAuthData(user: any): user is AuthData {
+//   return user && 'accessToken' in user;
+// }
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthData } from 'src/app/interface/auth-data.interface';
@@ -117,6 +174,7 @@ export class NewsletterComponent implements OnInit {
   profilo: User | undefined;
   successMessage: string | null = null;
   errorMessage: string | null = null;
+  isAdmin: boolean = false;
 
   constructor(private authSrv: AuthService, private profiloSrv: ProfiloService, private translate: TranslateService) { }
 
@@ -124,6 +182,7 @@ export class NewsletterComponent implements OnInit {
     this.authSrv.user$.subscribe((data) => {
       if (data && isAuthData(data)) {
         this.profilo = data.user;
+        this.isAdmin = data.user.tipoUtente.includes('ADMIN'); // Supponendo che i ruoli siano un array di stringhe
       }
     });
   }
